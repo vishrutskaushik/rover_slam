4bb2bdea3d8f966c0e2989dfe80bdbb1e90098a4
Vishrut Kaushik <vishrutskaushik@gmail.com>
2022-07-26 13:59:51 -0400

    Working 3D HDL SLAM on rosbag

---
+++ src/pointcloud_to_2dmap/CMakeFiles/pointcloud_to_2dmap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointcloud_to_2dmap.dir/src/pointcloud_to_2dmap.cpp.o"
+  "pointcloud_to_2dmap"
+  "pointcloud_to_2dmap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointcloud_to_2dmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
